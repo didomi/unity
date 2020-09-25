62e197b20e1165ef74808093db9848a451f593cc
ibrahim demir <ibrahimdemir.mail@gmail.com>
2020-09-26 00:12:34 +0300

    Build path for building apk and Mac file seperator fixed.

--- source/Assets/Plugins/Didomi/Editor/PostProcessor.cs
+++ source/Assets/Plugins/Didomi/Editor/PostProcessor.cs
@@ -1,107 +1,26 @@
 ﻿using System.IO;
 using System.Text;
 using UnityEditor;
+using UnityEditor.Android;
 using UnityEditor.Callbacks;
 using UnityEditor.iOS.Xcode;
 using UnityEngine;
 
+
 /// <summary>
-/// The PostProcessor updates the generated project for Android and iOS.
-/// We update native mobile projects to work with the Didomi SDKs (activity type, build settings, etc.).
-/// The "PostProcessBuild" attribute on Unity allows developers to add post-processing when Unity generates
-/// the Android Studio and Xcode projects.
+/// The PostProcessorGradleAndroidProject updates the generated project for Android.
+/// We post process generated projects to work with the Didomi SDKs (activity type, build settings, etc.).
+/// The "IPostGenerateGradleAndroidProject" interface on Unity allows developers to add post-processing when Unity generates
+/// apk and Android Studio projects.
 /// </summary>
-public static class PostProcessor
+class PostProcessorGradleAndroidProject : IPostGenerateGradleAndroidProject
 {
-    /// <summary>
-    /// Path of the folder that contains the `didomi_config.json` file in Unity projects.
-    /// </summary>
-    private static readonly string DidomiConfigPath = Application.dataPath + @"\DidomiConfig";
-	/// <summary>
-    /// Path of the `package.json` file.
-    /// </summary>
-	private static readonly string PackageJsonPath = Application.dataPath + @"\Plugins\Didomi\Resources\package.json";
-
-    /// <summary>
-    /// Method called when Unity generates native projects.
-    /// </summary>
-    /// <param name="buildTarget">Platform (Android, iOS)</param>
-    /// <param name="buildPath">Generated project path</param>
-    [PostProcessBuild]
-    public static void OnPostProcessBuild(BuildTarget buildTarget, string buildPath)
+    public int callbackOrder { get { return 0; } }
+    public void OnPostGenerateGradleAndroidProject(string path)
     {
-        Debug.Log("OnPostProcessBuild for " + buildTarget);
-        if (buildTarget == BuildTarget.iOS)
-        {
-            // PBXProject.GetPBXProjectPath returns the wrong path, we need to construct path by ourselves instead
-            // var projPath = PBXProject.GetPBXProjectPath(buildPath);
-            var projPath = buildPath + "/Unity-iPhone.xcodeproj/project.pbxproj";
-            var proj = new PBXProject();
-            proj.ReadFromFile(projPath);
-
-            var targetGuid = proj.GetUnityMainTargetGuid();
-
-            //// Configure build settings
-            proj.SetBuildProperty(targetGuid, "ENABLE_BITCODE", "NO");
-        
-            proj.AddBuildProperty(targetGuid, "DEFINES_MODULE", "YES");
-            proj.AddBuildProperty(targetGuid, "ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES", "YES");
-
-            proj.AddBuildProperty(targetGuid, "LD_RUNPATH_SEARCH_PATHS", "@executable_path/Frameworks");
-            proj.AddBuildProperty(targetGuid, "FRAMERWORK_SEARCH_PATHS", "$(inherited) $(PROJECT_DIR) $(PROJECT_DIR)/Frameworks");
-            proj.AddBuildProperty(targetGuid, "DYLIB_INSTALL_NAME_BASE", "@rpath");
-            proj.AddBuildProperty(targetGuid, "LD_DYLIB_INSTALL_NAME", "@executable_path/../Frameworks/$(EXECUTABLE_PATH)");
-          
-            CopyDidomiConfigFileToIOSFolder(proj, targetGuid, buildPath);
-			CopyPackageJsonToIOSFolder(proj, targetGuid, buildPath);
-
-            proj.WriteToFile(projPath);
-        }
-        else if (buildTarget == BuildTarget.Android)
-        {
-            AndroidPostProcess(buildPath);
-        }
-    }
-
-    /// <summary>
-    /// For iOS, copy the local `didomi_config.json` file to `Data/Resources/`.
-    /// It also gets added to the build to be available in the final app.
-    /// </summary>
-    /// <param name="project"></param>
-    /// <param name="targetGuid"></param>
-    /// <param name="path"></param>
-    private static void CopyDidomiConfigFileToIOSFolder(PBXProject project, string targetGuid, string path)
-    {
-        var files = Directory.GetFiles(DidomiConfigPath);
-
-        foreach (var filePath in files)
-        {
-            var fileName = Path.GetFileName(filePath);
-            var newCopyFile = @"Data\Resources\" + fileName;
-            var newCopyFileAbsolutePath = Path.Combine(path, newCopyFile);
-
-            File.Copy(filePath, newCopyFileAbsolutePath, true);
-            var fileGuid = project.AddFile(newCopyFile, newCopyFile);
-            project.AddFileToBuild(targetGuid, fileGuid);
-        }
-    }
-	
-	/// <summary>
-    /// For iOS, copy the `package.json` file to `Data/Resources/`.
-    /// It also gets added to the build to be available in the final app.
-    /// </summary>
-    /// <param name="project"></param>
-    /// <param name="targetGuid"></param>
-    /// <param name="path"></param>
-	private static void CopyPackageJsonToIOSFolder(PBXProject project, string targetGuid, string path)
-    {
-        var fileName = Path.GetFileName(PackageJsonPath);
-        var newCopyFile = @"Data\Resources\" + fileName;
-        var newCopyFileAbsolutePath = Path.Combine(path, newCopyFile);
-
-        File.Copy(PackageJsonPath, newCopyFileAbsolutePath, true);
-        var fileGuid = project.AddFile(newCopyFile, newCopyFile);
-        project.AddFileToBuild(targetGuid, fileGuid);
+        Debug.Log("Didomi OnPostGenerateGradleAndroidProject invoked " + path);
+        PostProcessorSettings.InitSettings();
+        AndroidPostProcess(path);
     }
 
     /// <summary>
@@ -113,7 +32,7 @@ public static class PostProcessor
         UpdateUnityPlayerActivity(path);
         UpdateUnityLibraryDependencies(path);
         CopyDidomiConfigFileToAssetFolder(path);
-		CopyPackageJsonFileToAssetFolder(path);
+        CopyPackageJsonFileToAssetFolder(path);
         UpdateStylesThemeToAppCompat(path);
         UpdateThemeAppCompatInAndroidManifestFile(path);
     }
@@ -124,7 +43,7 @@ public static class PostProcessor
     /// <param name="path"></param>
     private static void UpdateThemeAppCompatInAndroidManifestFile(string path)
     {
-        var unityAndroidmManifestFile = @"unityLibrary\src\main\AndroidManifest.xml";
+        var unityAndroidmManifestFile = $@"src{PostProcessorSettings.FilePathSeperator}main{PostProcessorSettings.FilePathSeperator}AndroidManifest.xml";
         var unityAndroidmManifestFileAbsolutePath = Path.Combine(path, unityAndroidmManifestFile);
 
         var lines = File.ReadAllLines(unityAndroidmManifestFileAbsolutePath);
@@ -153,47 +72,47 @@ public static class PostProcessor
     /// <param name="path"></param>
     private static void UpdateStylesThemeToAppCompat(string path)
     {
-        var unityPlayerFile = @"unityLibrary\src\main\res\values\styles.xml";
+        var unityPlayerFile = $@"src{PostProcessorSettings.FilePathSeperator}main{PostProcessorSettings.FilePathSeperator}res{PostProcessorSettings.FilePathSeperator}values{PostProcessorSettings.FilePathSeperator}styles.xml";
         var unityPlayerFileAbsolutePath = Path.Combine(path, unityPlayerFile);
         var oldValue = @"</resources>";
         var newValue = @"<style name=""DidomiTheme"" parent =""Theme.AppCompat.Light.DarkActionBar"" />
 
 </resources>";
 
-        ReplaceLineInFile(unityPlayerFileAbsolutePath, oldValue, newValue);
+        PostProcessor.ReplaceLineInFile(unityPlayerFileAbsolutePath, oldValue, newValue);
     }
 
-	/// <summary>
-    /// For Android, copy the local `didomi_config.json` file to `unityLibrary\src\main\assets\`.
+    /// <summary>
+    /// For Android, copy the local `didomi_config.json` file to `unityLibrary/src/main/assets/`.
     /// </summary>
-	/// <param name="path"></param>
+    /// <param name="path"></param>
     private static void CopyDidomiConfigFileToAssetFolder(string path)
     {
-        var files = Directory.GetFiles(DidomiConfigPath);
+        var files = Directory.GetFiles(PostProcessorSettings.DidomiConfigPath);
 
         foreach (var filePath in files)
         {
             var fileName = Path.GetFileName(filePath);
 
-            var newCopyFile = @"unityLibrary\src\main\assets\" + fileName;
+            var newCopyFile = $@"src{PostProcessorSettings.FilePathSeperator}main{PostProcessorSettings.FilePathSeperator}assets{PostProcessorSettings.FilePathSeperator}" + fileName;
             var newCopyFileAbsolutePath = Path.Combine(path, newCopyFile);
 
             File.Copy(filePath, newCopyFileAbsolutePath, true);
         }
     }
-	
-	/// <summary>
-    /// For Android, copy the local `package.json` to `unityLibrary\src\main\assets\`.
-    /// </summary>
-	/// <param name="path"></param>
-	private static void CopyPackageJsonFileToAssetFolder(string path)
-    {
-        var fileName = Path.GetFileName(PackageJsonPath);
 
-        var newCopyFile = @"unityLibrary\src\main\assets\" + fileName;
+    /// <summary>
+    /// For Android, copy the local `package.json` to `unityLibrary/src/main/assets/`.
+    /// </summary>
+    /// <param name="path"></param>
+    private static void CopyPackageJsonFileToAssetFolder(string path)
+    {
+        var fileName = Path.GetFileName(PostProcessorSettings.PackageJsonPath);
+
+        var newCopyFile = $@"src{PostProcessorSettings.FilePathSeperator}main{PostProcessorSettings.FilePathSeperator}assets{PostProcessorSettings.FilePathSeperator}" + fileName;
         var newCopyFileAbsolutePath = Path.Combine(path, newCopyFile);
 
-        File.Copy(PackageJsonPath, newCopyFileAbsolutePath, true);
+        File.Copy(PostProcessorSettings.PackageJsonPath, newCopyFileAbsolutePath, true);
     }
 
     /// <summary>
@@ -202,7 +121,7 @@ public static class PostProcessor
     /// <param name="path"></param>
     private static void UpdateUnityLibraryDependencies(string path)
     {
-        var unityPlayerFile = @"unityLibrary\build.gradle";
+        var unityPlayerFile = $@"build.gradle";
         var unityPlayerFileAbsolutePath = Path.Combine(path, unityPlayerFile);
         var oldValue = "dependencies {";
         var newValue = @"dependencies {
@@ -218,7 +137,7 @@ public static class PostProcessor
     implementation ""org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version""
     api 'com.iab.gdpr_android:gdpr_android:1.0.1'
     api 'com.google.code.gson:gson:2.8.5'";
-        ReplaceLineInFile(unityPlayerFileAbsolutePath, oldValue, newValue);
+        PostProcessor.ReplaceLineInFile(unityPlayerFileAbsolutePath, oldValue, newValue);
     }
 
     /// <summary>
@@ -228,11 +147,101 @@ public static class PostProcessor
     /// <param name="path"></param>
     private static void UpdateUnityPlayerActivity(string path)
     {
-        var unityPlayerFile = @"unityLibrary\src\main\java\com\unity3d\player\UnityPlayerActivity.java";
+        var unityPlayerFile = $@"src{PostProcessorSettings.FilePathSeperator}main{PostProcessorSettings.FilePathSeperator}java{PostProcessorSettings.FilePathSeperator}com{PostProcessorSettings.FilePathSeperator}unity3d{PostProcessorSettings.FilePathSeperator}player{PostProcessorSettings.FilePathSeperator}UnityPlayerActivity.java";
         var unityPlayerFileAbsolutePath = Path.Combine(path, unityPlayerFile);
         var oldValue = "public class UnityPlayerActivity extends Activity implements IUnityPlayerLifecycleEvents";
         var newValue = $"import android.support.v7.app.AppCompatActivity;{System.Environment.NewLine}{System.Environment.NewLine}public class UnityPlayerActivity extends AppCompatActivity implements IUnityPlayerLifecycleEvents";
-        ReplaceLineInFile(unityPlayerFileAbsolutePath, oldValue, newValue);
+        PostProcessor.ReplaceLineInFile(unityPlayerFileAbsolutePath, oldValue, newValue);
+    }
+}
+
+/// <summary>
+/// The PostProcessor updates the generated project for iOS.
+/// We update XCode mobile projects to work with the Didomi SDKs (activity type, build settings, etc.).
+/// The "PostProcessBuild" attribute on Unity allows developers to add post-processing when Unity generates
+/// for Xcode projects.
+/// </summary>
+public static class PostProcessor
+{
+    /// <summary>
+    /// Method called when Unity generates native projects.
+    /// </summary>
+    /// <param name="buildTarget">Platform (Android, iOS)</param>
+    /// <param name="buildPath">Generated project path</param>
+    [PostProcessBuild]
+    public static void OnPostProcessBuild(BuildTarget buildTarget, string buildPath)
+    {
+        Debug.Log("Didomi OnPostProcessBuild invoked" + buildPath);
+
+        if (buildTarget == BuildTarget.iOS)
+        {
+            PostProcessorSettings.InitSettings();
+
+            // PBXProject.GetPBXProjectPath returns the wrong path, we need to construct path by ourselves instead
+            // var projPath = PBXProject.GetPBXProjectPath(buildPath);
+            var projPath = buildPath + $"{PostProcessorSettings.FilePathSeperator}Unity-iPhone.xcodeproj{PostProcessorSettings.FilePathSeperator}project.pbxproj";
+            var proj = new PBXProject();
+            proj.ReadFromFile(projPath);
+
+            var targetGuid = proj.GetUnityMainTargetGuid();
+
+            //// Configure build settings
+            proj.SetBuildProperty(targetGuid, "ENABLE_BITCODE", "NO");
+
+            proj.AddBuildProperty(targetGuid, "DEFINES_MODULE", "YES");
+            proj.AddBuildProperty(targetGuid, "ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES", "YES");
+
+            proj.AddBuildProperty(targetGuid, "LD_RUNPATH_SEARCH_PATHS", "@executable_path/Frameworks");
+            proj.AddBuildProperty(targetGuid, "FRAMERWORK_SEARCH_PATHS", "$(inherited) $(PROJECT_DIR) $(PROJECT_DIR)/Frameworks");
+            proj.AddBuildProperty(targetGuid, "DYLIB_INSTALL_NAME_BASE", "@rpath");
+            proj.AddBuildProperty(targetGuid, "LD_DYLIB_INSTALL_NAME", "@executable_path/../Frameworks/$(EXECUTABLE_PATH)");
+
+            CopyDidomiConfigFileToIOSFolder(proj, targetGuid, buildPath);
+            CopyPackageJsonToIOSFolder(proj, targetGuid, buildPath);
+
+            proj.WriteToFile(projPath);
+        }
+    }
+
+    /// <summary>
+    /// For iOS, copy the local `didomi_config.json` file to `Data/Resources/`.
+    /// It also gets added to the build to be available in the final app.
+    /// </summary>
+    /// <param name="project"></param>
+    /// <param name="targetGuid"></param>
+    /// <param name="path"></param>
+    private static void CopyDidomiConfigFileToIOSFolder(PBXProject project, string targetGuid, string path)
+    {
+        var files = Directory.GetFiles(PostProcessorSettings.DidomiConfigPath);
+
+        foreach (var filePath in files)
+        {
+            var fileName = Path.GetFileName(filePath);
+            var newCopyFile = $@"Data{PostProcessorSettings.FilePathSeperator}Resources{PostProcessorSettings.FilePathSeperator}" + fileName;
+            var newCopyFileAbsolutePath = Path.Combine(path, newCopyFile);
+
+            File.Copy(filePath, newCopyFileAbsolutePath, true);
+            var fileGuid = project.AddFile(newCopyFile, newCopyFile);
+            project.AddFileToBuild(targetGuid, fileGuid);
+        }
+    }
+	
+	/// <summary>
+    /// For iOS, copy the `package.json` file to `Data/Resources/`.
+    /// It also gets added to the build to be available in the final app.
+    /// </summary>
+    /// <param name="project"></param>
+    /// <param name="targetGuid"></param>
+    /// <param name="path"></param>
+	private static void CopyPackageJsonToIOSFolder(PBXProject project, string targetGuid, string path)
+    {
+        var fileName = Path.GetFileName(PostProcessorSettings.PackageJsonPath);
+        var newCopyFile = $@"Data{PostProcessorSettings.FilePathSeperator}Resources{PostProcessorSettings.FilePathSeperator}" + fileName;
+        var newCopyFileAbsolutePath = Path.Combine(path, newCopyFile);
+
+        File.Copy(PostProcessorSettings.PackageJsonPath, newCopyFileAbsolutePath, true);
+        var fileGuid = project.AddFile(newCopyFile, newCopyFile);
+        project.AddFileToBuild(targetGuid, fileGuid);
     }
 
     /// <summary>
@@ -241,7 +250,62 @@ public static class PostProcessor
     /// <param name="path"></param>
     /// <param name="oldValue"></param>
     /// <param name="newValue"></param>
-    private static void ReplaceLineInFile(string path, string oldValue, string newValue)
+    public static void ReplaceLineInFile(string path, string oldValue, string newValue)
+    {
+        string text = File.ReadAllText(path);
+        text = text.Replace(oldValue, newValue);
+        File.WriteAllText(path, text);
+    }
+}
+
+/// <summary>
+/// Initial settings for PostProcessors
+/// </summary>
+public static class PostProcessorSettings
+{
+    /// <summary>
+    /// Path of the folder that contains the `didomi_config.json` file in Unity projects.
+    /// </summary>
+    public static string DidomiConfigPath = string.Empty;
+
+    /// <summary>
+    /// Path of the `package.json` file.
+    /// </summary>
+    public static string PackageJsonPath = string.Empty;
+
+    /// <summary>
+    /// File path seperator for Editor OS
+    /// </summary>
+    public static string FilePathSeperator = string.Empty;
+
+    /// <summary>
+    /// Initialize Environment Settings
+    /// </summary>
+    public static void InitSettings()
+    {
+        bool isWinEditor = Application.platform == RuntimePlatform.WindowsEditor;
+        bool isOSXEditor = Application.platform == RuntimePlatform.OSXEditor;
+
+        if (isOSXEditor)
+        {
+            FilePathSeperator = "/";
+        }
+        else
+        {
+            FilePathSeperator = @"\";
+        }
+
+        DidomiConfigPath = Application.dataPath + $@"{FilePathSeperator}DidomiConfig";
+        PackageJsonPath = Application.dataPath + $@"{FilePathSeperator}Plugins{FilePathSeperator}Didomi{FilePathSeperator}Resources{FilePathSeperator}package.json";
+    }
+
+    /// <summary>
+    /// Replace arguments in path
+    /// </summary>
+    /// <param name="path"></param>
+    /// <param name="oldValue"></param>
+    /// <param name="newValue"></param>
+    public static void ReplaceLineInFile(string path, string oldValue, string newValue)
     {
         string text = File.ReadAllText(path);
         text = text.Replace(oldValue, newValue);
