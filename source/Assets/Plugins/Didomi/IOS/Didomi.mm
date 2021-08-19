a7948afcfdb789e2f50ece534ff7030266a10544
Philemon Merlet <philemon.merlet@didomi.io>
2021-08-19 14:19:01 +0200

    Improve comment

--- source/Assets/Plugins/Didomi/IOS/Didomi.mm
+++ source/Assets/Plugins/Didomi/IOS/Didomi.mm
@@ -318,7 +318,7 @@ char* convertNSStringToCString(NSString * _Nullable nsString)
         return NULL;
 
     const char* nsStringUtf8 = [nsString UTF8String];
-    //create a null terminated C string on the heap so that our string's memory isn't wiped out right after method's return
+    // Create a null terminated C string on the heap so that our string's memory isn't wiped out right after method's return
     char* cString = (char*)malloc(strlen(nsStringUtf8) + 1);
     strcpy(cString, nsStringUtf8);
 
