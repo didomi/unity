71a4597c41b5692c5c819891e70452a5f6b53257
Philemon Merlet <philemon.merlet@didomi.io>
2021-12-03 18:14:41 +0100

    Revert incorrect change on demo GUI script

--- source/Assets/Scripts/DemoGUI.cs
+++ source/Assets/Scripts/DemoGUI.cs
@@ -60,8 +60,8 @@ public class DemoGUI : MonoBehaviour
         labelResult = GameObject.Find("Result");
         panel= GameObject.Find("Panel");
         SetResponsiveLayout();
-        //InitializeDidomi();
-        //Didomi.GetInstance().SetupUI();
+        InitializeDidomi();
+        Didomi.GetInstance().SetupUI();
     }
 
     void Update()
