71ac3c0e29ac508a99b4106eb040cc70428ec926
Philemon Merlet <philemon.merlet@didomi.io>
2023-04-19 15:52:03 +0200

    Fixes after code review

--- source/scripts/runTests.sh
+++ source/scripts/runTests.sh
@@ -4,6 +4,8 @@ UNITY_PATH="/Applications/Unity/Hub/Editor/2019.4.36f1/Unity.app/Contents/MacOS/
 RESULTS_PATH="test-results.xml"
 LOG_PATH="testsRun.log"
 
+# Run the tests for Didomi Plugin on iOS
+# Note: Currently Unity does not correctly run tests on Android 
 $UNITY_PATH -batchmode -runTests -testResults $RESULTS_PATH -testPlatform iOS -logFile $LOG_PATH
 
 if [ $? -eq 0 ]
