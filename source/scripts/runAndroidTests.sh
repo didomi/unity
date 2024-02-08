718b5092c56f35663197fd9cc27804c48e0c6e82
Nicolas Chaix <nicolas.chaix@didomi.io>
2024-02-08 11:55:31 +0100

    check for device before launching android test / launch emulator if not

--- source/scripts/runAndroidTests.sh
+++ source/scripts/runAndroidTests.sh
@@ -13,6 +13,33 @@ UNITY_PATH=$(awk -F= '/^editor/ {print $2}' $scriptsDir/unity.properties)
 RESULTS_PATH="artifacts/android-test-results.xml"
 LOG_PATH="artifacts/androidTestsRun.log"
 
+# Check for any Android emulator / device
+connected_devices=$(adb devices | grep -v "List" | grep "device$")
+if [ -z "$connected_devices" ]; then
+    echo "No Android device connected."
+    
+    # Search for device name
+    DEVICE=$(emulator -list-avds | head -n 1)
+
+    echo "Launching $DEVICE, please wait a few seconds..."
+    ~/Library/Android/sdk/emulator/emulator "@$DEVICE" &
+
+    # Wait for the Android device to be detected
+    adb wait-for-device
+
+    echo "Device detected. Waiting for device to fully boot..."
+
+    # Wait until the boot animation completes
+    until adb shell getprop sys.boot_completed | grep -m 1 '1'; do
+        echo "Waiting for $DEVICE to complete booting..."
+        sleep 1
+    done
+
+    echo "$DEVICE is ready."
+else
+    echo "Android device(s) connected, continue with tests."
+fi
+
 # Run the tests
 $UNITY_PATH -batchmode -buildTarget Android -runTests -testResults $RESULTS_PATH -testPlatform Android -logFile $LOG_PATH
 
